b02281c375e0459e0fbf1ae326ced358a5c0fcef
nwaon005 <lolgfman@gmail.com>
2018-11-13 10:28:13 -0600

    Finish comments

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,9 +1,10 @@
 package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
-
+    //We need the result to be a max value so that we can have a result of
+    //any value.
     private long result = Integer.MAX_VALUE;
-
+	//Synchronized method for updating the overall global result. This method is called once by each thread after they have finished cycling through their data.
     synchronized private void updateGlobalResult(long newResult) {
         if(newResult < result) {
             result = newResult;
@@ -13,19 +14,23 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 
     @Override
     public long minimumPairwiseDistance(int[] values){
-
+	//n in the formula is equal to the maximum length of the values.
         int n = values.length;
 
+	//Here we initialize the threads for each section that we want to get
+	//values of.
         Thread threadLL = new Thread(new PairwiseThreadLL(values));
         Thread threadBR = new Thread(new PairwiseThreadBR(values));
         Thread threadTR = new Thread(new PairwiseThreadTR(values));
         Thread threadC = new Thread(new PairwiseThreadC(values));
-
+	
+	//Here we start each thread section.
         threadLL.start();
         threadBR.start();
         threadTR.start();
         threadC.start();
-
+	
+	//Here we wait for the threads to join.
         try {
             threadLL.join();
             threadBR.join();
@@ -39,7 +44,8 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
         return result;
 
     }
-
+	
+    //This is a constructor class for each thread that implements runnable. It saves the array of values in the constructor.
     class PairwiseThreadLL implements Runnable {
         private int[] values;
         private int n;
@@ -48,7 +54,8 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
             this.values = values;
             this.n = values.length;
         }
-
+	
+	//We implement the algorithm of how the Lower left section of the data.
         public void run() {
             long localResult = Integer.MAX_VALUE;
             for (int i = 1; i < n/2; ++i) {
@@ -72,7 +79,7 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
             this.values = values;
             this.n = values.length;
         }
-
+	//We implement the algorithm of how the Bottom Right section of the data.
         public void run() {
             long localResult = Integer.MAX_VALUE;
             for (int i = n/2 + 1; i < n; ++i) {
@@ -96,7 +103,7 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
             this.values = values;
             this.n = values.length;
         }
-
+	//We implement the algorithm of how the Top Right section of the data.
         public void run() {
             long localResult = Integer.MAX_VALUE;
             for (int i = 1 + n/2; i < n; ++i) {
@@ -120,7 +127,7 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
             this.values = values;
             this.n = values.length;
         }
-
+	//We implement the algorithm of how the Center section of the data.
         public void run() {
             long localResult = Integer.MAX_VALUE;
             for (int j = 0; j < n/2; ++j) {
